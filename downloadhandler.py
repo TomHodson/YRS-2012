c2c57a70ea1c8d4b0bae6bdd257b8d87563a1b53
TomHodson <thomas.c.hodson@gmail.com>
2012-08-06 20:25:59 +0100

    remove downloadhandler.py

--- downloadhandler.py
+++
@@ -1,8 +0,0 @@
-from downloader import downloader
-from multiprocessing import Process,Queue
-
-rawtweets = Queue()
-downloaderp = Process(target = downloader, args=(rawtweets,))
-sanitiserp = Process(target = sansi)
-while True:
-    print queue.get(True).body
