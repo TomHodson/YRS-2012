9869aebe919181e1bc3da3816d3fff49b0e9a5b2
Alex Owen-Meehan <alexator121@gmail.com>
2012-08-06 16:38:27 +0100

    Alex sanitizer with queue interface

--- Cleanup Function.py
+++ Cleanup Function.py
@@ -1,11 +1,14 @@
 import re
 import string
-def cleanupfunction(tweet):
+def cleanupfunction(inqueue, outqueue):
+    tweetobj = inqueue.get(True)
+    tweet = tweetobj.body
     tweet = tweet.translate(string.maketrans("","",),"(){}[]")
     tweet = re.sub("[%s]*[%s]+[%s]+" % (string.ascii_letters, string.punctuation, string.ascii_letters),"",tweet)
     tweet = tweet.translate(string.maketrans("","",),string.punctuation)
     tweet = re.sub(" +"," ",tweet)
-    print tweet
+    tweetobj.body = tweet
+    outqueue.put(tweet)
 
 
 
