94dafa7f9d88511c656b8dcc3950f29fbc91d8c5
TomHodson <thomas.c.hodson@gmail.com>
2012-08-07 12:07:07 +0100

    merged parsing functions

--- parser.py
+++ parser.py
@@ -1,52 +1,42 @@
-from collections import Counter
+from collections import Counter, defaultdict, namedtuple
+from itertools import permutations
 
-def consecutiveparser(inqueue,outqueue,kill):
+def parser(inqueue,outqueue,kill):
     while not kill == 1: #forever
         try:
             tweet = inqueue.get(True) #read tweet object
         except IOError:
             print "parser out"
             return
-        body = tweet.body #get tweet data
-        single = Counter()
-        for word in body: #get count of single words
-            single[word] += 1
-        double = Counter()
-        for i in range(len(body)-1): #get count of word pairs
-            pair = body[i:i+2]
-            pair.sort()
-            pair = tuple(pair)
-            double[pair] += 1
-        tweet.single = dict(single)
-        tweet.double = dict(double)
-        inqueue.task_done()
-        outqueue.put(tweet) #output to new queue
+        tweet.body #get tweet data
 
-from collections import defaultdict, Counter
-from itertools import combinations
-
-def intweetparser(inputqueue, outputqueue):
-    while True:
-        tweet  = inputqueue.get(block = True)
-        words = sorted(tweet.body)
+    #make singles data
         tweet.single = Counter()
-        tweet.double = Counter()
-        
-        for word in words:
+        for word in tweet.body: #get count of single words
             tweet.single[word] += 1
 
-        wordpairs = combinations(words, 2) #(word1, word2) where word1 != word2 and order does NOT matter
-        def f(x): tweet.double[x] += 1
-        map(f, wordpairs)
-        tweet.single, tweet.double = dict(tweet.single), dict(tweet.double)
-        outputqueue.put(tweet)
-
-parser = consecutiveparser
+    #make doubles data
+    #IMPORTANT data is in the format {(word1, word2) : [consecutive, intweetcount]}
+        tweet.double = defaultdict(lambda :[0,0]) #initialise the double attribute
+        #make consecutive data    
+        for i in range(len(tweet.body)-1): #get count of word pairs
+            pair = tuple(tweet.body[i:i+2])
+            tweet.double[pair][0] += 1
+        #make intweetdata
+        wordpairs = permutations(tweet.body, 2) #all the ways of picking two words from the tweet where word1 != word2
+        def f(x): tweet.double[x][1] += 1
+        map(f, wordpairs) #using fp here for the lulz
+            
+        tweet.single = dict(tweet.single)
+        tweet.double = dict(tweet.double)
+        outqueue.put(tweet) #output to new queue
+        inqueue.task_done()
 
 #yay for testing
 if __name__ == '__main__':
     import time
-    from multiprocessing import Process, Queue
+    from multiprocessing import Process
+    from multiprocessing import JoinableQueue as Queue
     class Tweet():
         def __init__(self):
                 pass
@@ -56,20 +46,9 @@ if __name__ == '__main__':
     inputqueue.put(testtweet)
     outputqueue = Queue()
 
-    p = Process(target = consecutiveparser, args = (inputqueue, outputqueue))
+    p = Process(target = parser, args = (inputqueue, outputqueue, 0))
     p.start()
-    time.sleep(0.5) #DIRTY HACK
+    inputqueue.join()
     p.terminate()
     out = outputqueue.get()
-    print 'intweetparser: len={}, {}, {}'.format(len(out.double), out.single, out.double) 
-
-    inputqueue = Queue()
-    inputqueue.put(testtweet)
-    outputqueue = Queue()
-
-    p = Process(target = intweetparser, args = (inputqueue, outputqueue))
-    p.start()
-    time.sleep(0.5) #DIRTY HACK
-    p.terminate()
-    out = outputqueue.get()
-    print 'intweetparser: len={}, {}, {}'.format(len(out.double), out.single, out.double)
+    print 'parser: len={}, {}, {}'.format(len(out.double), out.single, out.double) 
