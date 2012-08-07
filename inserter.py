795fca908dfb2e2aeda51bb9fe303bbeee8ec592
psychomario <pink.banana.fish@gmail.com>
2012-08-07 14:04:51 +0100

    almost complete doubles except single error

--- inserter.py
+++ inserter.py
@@ -1,8 +1,16 @@
-#CREATE TABLE singles(id INTEGER PRIMARY KEY, word varchar(140) UNIQUE NOT NULL ON CONFLICT REPLACE, count INTEGER NOT NULL);
+#CREATE TABLE singles(id INTEGER PRIMARY KEY, word varchar(140) NOT NULL, count INTEGER NOT NULL);
 
-#SELECT word FROM singles WHERE word == "%s" LIMIT 1;
-#   INSERT INTO singles (word,count) VALUES ("%s", %d);
-#   UPDATE singles SET count = count + %d WHERE word = "%s";
+#SELECT word FROM singles WHERE word == '%s' LIMIT 1
+#   INSERT INTO singles (word,count) VALUES ('%s', %d)
+#   UPDATE singles SET count = count + %d WHERE word = '%s'
+
+#CREATE TABLE doubles(id INTEGER PRIMARY KEY, word1id INTEGER NOT NULL, word2id INTEGER NOT NULL, intweet INTEGER NOT NULL, consecutive INTEGER NOT NULL);
+
+#SELECT id FROM singles WHERE word == '%s' LIMIT 1 #word1id
+#SELECT id FROM singles WHERE word == '%s' LIMIT 1 #word2id
+#SELECT word1id,word2id FROM doubles WHERE word1id == '%s' AND word2id == '%s' LIMIT 1
+#   INSERT INTO doubles (word1id,word2id,intweet,consecutive) VALUES (%d, %d, %d, %d)
+#   UPDATE doubles SET intweet = intweet + %d, consecutive = consecutive + %d WHERE word1id == %d AND WHERE word2id == %d
 import sqlite3
 
 def inserter(inqueue,kill):
@@ -21,10 +29,31 @@ def inserter(inqueue,kill):
             query = "SELECT word FROM singles WHERE word == '%s' LIMIT 1" % word
             cursor.execute(query)
             if not cursor.fetchone():
-                query = "INSERT INTO singles (word,count) VALUES ('%s', %d)" % (word,tweets[word])
+                query = "INSERT INTO singles (word,count) VALUES ('%s', %d)" % (word,tweets.singles[word])
             else:
-                query = "UPDATE singles SET count = count + %d WHERE word = '%s'" % (tweets[word],word)
+                query = "UPDATE singles SET count = count + %d WHERE word = '%s'" % (tweets.singles[word],word)
             cursor.execute(query)
         database.commit()
-        cursor.execute("SELECT count(word) FROM singles")
-        print cursor.fetchone()
+        for pair in tweets.doubles:
+            word1,word2 = pair
+            consecutive,intweet = tweets.doubles[pair]
+            query = "SELECT id FROM singles WHERE word == '%s' LIMIT 1"
+            cursor.execute(query % word1)
+            word1id = cursor.fetchone()[0]
+            cursor.execute(query % word2)
+            word2id = cursor.fetchone()[0]
+            query = "SELECT word1id,word2id FROM doubles WHERE word1id == %d AND word2id == %d LIMIT 1"
+            query = query % (word1id,word2id)
+            cursor.execute(query)
+            if not cursor.fetchone():
+                query = "INSERT INTO doubles (word1id,word2id,intweet,consecutive) VALUES (%d, %d, %d, %d)"
+                query = query % (word1id,word2id,intweet,consecutive)
+            else:
+                query = "UPDATE doubles SET intweet = intweet + %d, consecutive = consecutive + %d WHERE word1id == %d AND WHERE word2id == %d"
+                query = query % (intweet,consecutive,word1id,word2id)
+            try:
+                cursor.execute(query)
+            except:
+                print query
+        database.commit()
+        print "set"
