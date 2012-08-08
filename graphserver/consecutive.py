122af9792f8c1f2f73d03f33836e535f7870f5af
psychomario <pink.banana.fish@gmail.com>
2012-08-08 15:10:13 +0100

    finished first graphing function

--- graphserver/consecutive.py
+++ graphserver/consecutive.py
@@ -3,8 +3,8 @@ database = sqlite3.connect("../data.db")
 cursor = database.cursor()
 
 full = """{"nodes":[%s],"links":[%s]}"""
-node = """{"name":"%s","color":"#fff", "size": %d, "id":%d}"""
-link = """{"source":%d,"target":%d,"value":%d, "color":"#fff"}"""
+node = """{"name":"%s","color":"#f00", "size": %d, "id":%d}"""
+link = """{"source":%d,"target":%d,"value":%d, "color":"#f00"}"""
 
 def getconnwords(wid):
     query = "SELECT word2id,consecutive FROM doubles WHERE consecutive > 0 AND (word1id == %d OR word2id == %d)" % (wid,wid)
@@ -12,9 +12,9 @@ def getconnwords(wid):
     return cursor.fetchall()
 
 def getidcount(wid):
-    cursor.execute("SELECT word,count FROM singles WHERE id == %d" % wid)
-    word,count = cursor.fetchone()
-    return word,count
+    cursor.execute("SELECT word,prob FROM singles WHERE id == %d" % wid)
+    word,prob = cursor.fetchone()
+    return word,prob
 
 def getnodeindex(cwid,allnodes):
     for index,node in enumerate(allnodes):
@@ -22,22 +22,24 @@ def getnodeindex(cwid,allnodes):
             return index
 
 def consecutive(word):
-    cursor.execute("SELECT id,count FROM singles WHERE word == '%s'" % word)
-    wid,count = cursor.fetchone()
+    cursor.execute("SELECT id,prob FROM singles WHERE word == '%s'" % word)
+    wid,prob = cursor.fetchone()
     if not wid: return '{"ERROR":"word not found"}'
     allnodes = []
     alllinks = []
     allwidsdup = getconnwords(wid)
     allwids=[]
+    allnodes.append(node % (word,prob,wid))
     for i in allwidsdup:
         if i not in allwids:
             allwids.append(i)
     for cwid,consec in allwids:
         word,count = getidcount(cwid)
-        cnode = node % (word, count,cwid)
+        cnode = node % (word.encode('ascii','ignore'), count,cwid)
         allnodes.append(cnode)
     for cwid,consec in allwids:
         clink = link % (0,getnodeindex(cwid,allnodes),consec)
-        if clink not in alllinks: allinks.append(clink)
+        if clink not in alllinks: alllinks.append(clink)
+    return full % (',\n'.join(allnodes),',\n'.join(alllinks))
         
-consecutive("you")
+print consecutive("you")
