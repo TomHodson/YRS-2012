fee5138415b6f8e5b2a25f0ef485bbc26471a71a
psychomario <pink.banana.fish@gmail.com>
2012-08-08 14:26:49 +0100

    added functions to consecutive

--- graphserver/consecutive.py
+++ graphserver/consecutive.py
@@ -1,4 +1,20 @@
 import sqlite3
 
+def getconnwords(wid):
+    query = "SELECT word2id,consecutive FROM doubles WHERE consecutive > 0 AND word1id == '%d' AND (word2id =='" % wid
+    cursor.execute(query+"' OR word2id == '".join(allwids)+"')")
+    return cursor.fetchall()
+
+def getidcount(wid):
+    cursor.execute("SELECT word,count FROM singles WHERE id == %s" % word)
+    word,count = cursor.fetchone()
+    return word,count
+
 def consecutive(word):
+    base = sqlite3.connect("../data.db")
+    cursor = database.cursor()
+
+    cursor.execute("SELECT id,count FROM singles WHERE word == %s" % word)
+    wid,count = cursor.fetchone()
+    if not wid: return '{"ERROR":"word not found"}'
     
