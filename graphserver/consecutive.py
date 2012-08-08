15c444754bff835ad85bd44ba32336ef7ceceb3c
psychomario <pink.banana.fish@gmail.com>
2012-08-08 14:56:59 +0100

    generated json, not printed yet

--- graphserver/consecutive.py
+++ graphserver/consecutive.py
@@ -1,20 +1,43 @@
 import sqlite3
+database = sqlite3.connect("../data.db")
+cursor = database.cursor()
+
+full = """{"nodes":[%s],"links":[%s]}"""
+node = """{"name":"%s","color":"#fff", "size": %d, "id":%d}"""
+link = """{"source":%d,"target":%d,"value":%d, "color":"#fff"}"""
 
 def getconnwords(wid):
-    query = "SELECT word2id,consecutive FROM doubles WHERE consecutive > 0 AND word1id == '%d' AND (word2id =='" % wid
-    cursor.execute(query+"' OR word2id == '".join(allwids)+"')")
+    query = "SELECT word2id,consecutive FROM doubles WHERE consecutive > 0 AND (word1id == %d OR word2id == %d)" % (wid,wid)
+    cursor.execute(query)
     return cursor.fetchall()
 
 def getidcount(wid):
-    cursor.execute("SELECT word,count FROM singles WHERE id == %s" % word)
+    cursor.execute("SELECT word,count FROM singles WHERE id == %d" % wid)
     word,count = cursor.fetchone()
     return word,count
 
-def consecutive(word):
-    base = sqlite3.connect("../data.db")
-    cursor = database.cursor()
+def getnodeindex(cwid,allnodes):
+    for index,node in enumerate(allnodes):
+        if node.endswith("id\":%d}" % cwid):
+            return index
 
-    cursor.execute("SELECT id,count FROM singles WHERE word == %s" % word)
+def consecutive(word):
+    cursor.execute("SELECT id,count FROM singles WHERE word == '%s'" % word)
     wid,count = cursor.fetchone()
     if not wid: return '{"ERROR":"word not found"}'
-    
+    allnodes = []
+    alllinks = []
+    allwidsdup = getconnwords(wid)
+    allwids=[]
+    for i in allwidsdup:
+        if i not in allwids:
+            allwids.append(i)
+    for cwid,consec in allwids:
+        word,count = getidcount(cwid)
+        cnode = node % (word, count,cwid)
+        allnodes.append(cnode)
+    for cwid,consec in allwids:
+        clink = link % (0,getnodeindex(cwid,allnodes),consec)
+        if clink not in alllinks: allinks.append(clink)
+        
+consecutive("you")
