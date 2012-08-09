71f409a54f990fb152e893dba9d9fbe7497ac0e9
TomHodson <thomas.c.hodson@gmail.com>
2012-08-09 23:14:44 +0100

    made a version of miner that should run all night hardtokillminer.py

---
+++ pipeline/hardtokillminer.py
@@ -0,0 +1,59 @@
+from downloader import downloader
+from sanitiser import sanitiser
+from analyzer import analyzer
+from merger import merger
+from inserter import inserter
+from forker import forker
+from markov import markov
+from multiprocessing import Process,JoinableQueue,Value,active_children
+from time import sleep
+from os import kill
+from signal import SIGKILL
+
+def cleanup():
+    killProc = 1
+    sleep(3)
+    downloadert.terminate()
+    forkert.terminate()
+    markovt.terminate()
+    sanitisert.terminate()
+    analyzert.terminate()
+    insertert.terminate()
+    for i in active_children():
+        kill(i.pid,SIGKILL)
+while True:
+    try:
+        fromDownloadQueue = JoinableQueue()
+        killProc = Value('d',0)
+        toSanitiser = JoinableQueue()
+        toMarkov = JoinableQueue()
+        fromSanitiserQueue = JoinableQueue()
+        fromAnalyzerQueue = JoinableQueue()
+        fromMergerQueue = JoinableQueue()
+
+        downloadert = Process(target = downloader, args=(fromDownloadQueue,killProc))
+        downloadert.start()
+
+        forkert = Process(target = forker, args=(fromDownloadQueue,toSanitiser,toMarkov,killProc))
+        forkert.start()
+
+        sanitisert = Process(target = sanitiser, args=(toSanitiser,fromSanitiserQueue,killProc))
+        sanitisert.start()
+        markovt = Process(target = markov, args=(toMarkov,killProc))
+        markovt.start()
+
+        analyzert = Process(target = analyzer, args=(fromSanitiserQueue,fromAnalyzerQueue,killProc))
+        analyzert.start()
+
+        mergert = Process(target = merger, args=(fromAnalyzerQueue,fromMergerQueue,killProc))
+        mergert.start()
+
+        insertert = Process(target = inserter, args=(fromMergerQueue,killProc))
+        insertert.start()
+
+        
+        while True:
+            sleep(100)
+    except:
+        cleanup()
+        continue
