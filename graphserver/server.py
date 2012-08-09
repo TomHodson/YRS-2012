f8bec1de2a4e7b0095af7f3772bc0d24cc3c461c
psychomario <pink.banana.fish@gmail.com>
2012-08-09 09:45:08 +0100

    updated server & pages to allow argument passing

--- graphserver/server.py
+++ graphserver/server.py
@@ -10,41 +10,24 @@ def dummy(request):
 graphfunctions = {'dummy' : dummy}
 
 class JSON(resource.Resource):
-<<<<<<< HEAD
-	isLeaf = True
-	
-	def render_GET(self, request):
-		print 'a get request'
-		
-		if not request.args: # if there are no args
-			request.setHeader("content-type", "text/html")
-			if request.path == '/':
-				return open('./index.html').read()
-			else:
-				try:	
-					return open('.{path}'.format(path=request.path)).read()
-				except IOError:
-					request.setResponseCode(404)
-					return 'Error'
-=======
     isLeaf = True
-    
+	
     def render_GET(self, request):
-        print 'a get request'
-    	
-        if not request.args: # if there are no args
-            request.setHeader("content-type", "text/html")
-            return open('mainpage.html').read()
->>>>>>> 5aa2f50c757ea1b8f0bbd134fd97ddf93497ce9a
-
-
+        if request.args and request.path.endswith(".json"):
+            seed = request.args['word'][0]
+            depth = request.args['depth'][0]
+            #HANDLE json REQUESTS HERE
+            return ""
         else:
-            request.setHeader("content-type", "application/json")
-
-            #function = graphfunctions[request.args['graphtype']]
-            if request.args['graphtype'][0] == "consec":
-                return "consec"
-            return function(request)
+            request.setHeader("content-type", "text/html")
+            if request.path == '/':
+                return open('./mainpage.html').read()
+            else:
+                try:    	
+                    return open('.{path}'.format(path=request.path)).read()
+                except IOError:
+                    request.setResponseCode(404)
+                    return 'Error'
    	
 
 
